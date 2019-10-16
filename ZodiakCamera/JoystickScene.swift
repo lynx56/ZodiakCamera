7a0b993cef0714650ae6929d6c965ae391e37df0
Gulnaz Almuhametova <666lynx666@mail.ru>
2019-10-16 22:50:39 +0300

    init

---
+++ ZodiakCamera/JoystickScene.swift
@@ -0,0 +1,97 @@
+//
+//  JoystickScene.swift
+//  ZodiakCamera
+//
+//  Created by lynx on 16/10/2019.
+//  Copyright © 2019 gulnaz. All rights reserved.
+//
+import Foundation
+import UIKit
+import SpriteKit
+
+class JoystickView: SKView {
+    enum MoveDirection: Int {
+        case up
+        case upleft
+        case left
+        case downleft
+        case down
+        case right
+        case downright
+        case upright
+    }
+    
+    private let joystickScene = JoystickScene()
+    var moveHandler: (MoveDirection) -> Void = { _ in }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        joystickScene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        joystickScene.handler = handleJoystickSceneEvent
+        self.presentScene(joystickScene)
+    }
+    
+    override func layoutSubviews() {
+        joystickScene.size = self.bounds.size
+    }
+    
+    private func handleJoystickSceneEvent(_ event: JoystickScene.Event) {
+        guard case let .move(angle) = event else { return }
+        
+        let pi = CGFloat(Double.pi)
+        let angles = [0, pi/4, pi/2, 3*pi/4, pi]
+        let result = [MoveDirection.up, .upleft, .left, .downleft, .down]
+        let mirrorResult = [MoveDirection.up, .upright, .right, .downright, .down]
+        
+        var min = 0
+        for i in 1..<angles.count {
+            if abs(angles[i]-abs(angle)) < abs(angles[min]-abs(angle)) {
+                min = i
+            }
+        }
+        
+        let moveDirection = angle < 0 ? mirrorResult[min] : result[min]
+        moveHandler(moveDirection)
+    }
+}
+
+class JoystickScene: SKScene {
+    private var joystick: TLAnalogJoystick?
+    
+    override var size: CGSize {
+        didSet {
+            joystick?.diameter = min(size.width, size.height)/2
+        }
+    }
+    
+    enum Event {
+        case begin
+        case move(CGFloat)
+        case end
+    }
+    
+    var handler: (Event) -> Void = { _ in }
+    
+    override func didMove(to view: SKView) {
+        joystick = TLAnalogJoystick(withDiameter: 50)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        backgroundColor = .white
+        joystick?.handleImage = UIImage(named: "stick")!
+        joystick?.baseImage = UIImage(named: "substrate")!
+        
+        addChild(joystick!)
+ 
+        joystick?.on(.begin) { [weak self] _ in
+            self?.handler(.begin)
+         }
+        
+        joystick!.on(.move) { [weak self] joystick in
+            self?.handler(.move(joystick.angular))
+        }
+        
+        joystick?.on(.end){ [weak self] _ in
+            self?.handler(.end)
+        }
+    }
+}
---
+++ ZodiakCamera/ViewController.swift
@@ -0,0 +1,221 @@
+//
+//  ViewController.swift
+//  ZodiakCamera
+//
+//  Created by lynx on 14/10/2019.
+//  Copyright © 2019 gulnaz. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var up: ControlButton!
+    @IBOutlet weak var down: ControlButton!
+    @IBOutlet weak var right: ControlButton!
+    @IBOutlet weak var left: ControlButton!
+    @IBOutlet weak var stop: ControlButton!
+    private var settings = Dictionary<Settings, Int>()
+    
+    @IBOutlet weak var joystickView: JoystickView!
+    let url = URL(string: "http://188.242.14.235:81/videostream.cgi?loginuse=admin&amp;loginpas=123123")
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+        up.handler = userManipulate
+        down.handler = userManipulate
+        right.handler = userManipulate
+        left.handler = userManipulate
+        stop.handler = userManipulate
+        
+        imagetask = URLSession.shared.dataTask(with: URL(string: "http://188.242.14.235:81/snapshot.cgi?user=admin&pwd=123123")!) { (data, response, error) in
+            if let data = data {
+                let image = UIImage(data: data)
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            }
+        }
+        
+        webView.isHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+       //  webView.load(.init(url: url!))
+       // readSetiings()
+      //  createDisplayLink()
+        
+        joystickView.moveHandler = {
+            print($0)
+        }
+    }
+    
+    func createDisplayLink() {
+        let displaylink = CADisplayLink(target: self,
+                                        selector: #selector(step))
+        
+        displaylink.add(to: .current,
+                        forMode: .default)
+    }
+    
+    var imagetask: URLSessionDataTask?
+    
+    @objc func step(displaylink: CADisplayLink) {
+        imageView.image = UIImage(data: try! Data(contentsOf: URL(string: "http://188.242.14.235:81/snapshot.cgi?user=admin&pwd=123123")!))
+        
+        //imagetask?.resume()
+    }
+    
+    func getUrl(with cgi: String) -> String {
+        return "http://188.242.14.235:81/\(cgi)?loginuse=admin&amp;loginpas=123123"
+    }
+    
+    func chageSettings(param: String, value: String) {
+        var cgi =  getUrl(with: "camera_control.cgi")
+        cgi += "&param=\(param)&value=\(value)"
+        cgi += "&\(Date().stamp())"
+    
+        let url = URL(string: cgi)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            print(error)
+        }
+        
+        task.resume()
+    }
+    
+    func userManipulate(command: String) {
+        var cgi = getUrl(with: "decoder_control.cgi");
+        cgi += "&command=\(command)"
+        cgi += "&onestep=0"
+        cgi += "&\(Date().stamp())"
+        
+        let url = URL(string: cgi)!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            print(error)
+        }
+        
+        task.resume()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    func readSetiings() {
+        let url = getUrl(with: "get_camera_params.cgi")
+        let task = URLSession.shared.downloadTask(with: URL(string: url)!) { (file, response, error) in
+            if let file = file {
+                self.settings = try! String(contentsOf: file).parseCGI()
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+@IBDesignable
+public class ControlButton: UIButton {
+    @IBInspectable var stop: Int = 0
+    @IBInspectable var execute: Int = 0
+    
+    var handler: (String)->Void = { _ in }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        addTarget(self, action: #selector(controlDown), for: .touchDown)
+        addTarget(self, action: #selector(controlUp), for: .touchUpInside)
+    }
+    
+    @objc func controlDown(_ sender: UIButton) {
+        handler("\(execute)")
+    }
+    
+    @objc func controlUp(_ sender: UIButton) {
+        handler("\(stop)")
+    }
+}
+
+@IBDesignable
+public class SetupButton: UIButton {
+    @IBInspectable var execute: String = ""
+    
+    var handler: (String)->Void = { _ in }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        addTarget(self, action: #selector(controlUp), for: .touchUpInside)
+    }
+    
+    @objc func controlUp(_ sender: UIButton) {
+        handler(execute)
+    }
+}
+
+
+
+extension Date {
+    func stamp() -> Int64! {
+        return Int64(self.timeIntervalSince1970 * 1000) + Int64(arc4random())
+    }
+}
+
+
+extension String {
+    func parseCGI()->Dictionary<Settings, Int> {
+        var result = Dictionary<Settings, Int>()
+        let components =
+            self
+                .removingAllWhitespaces()
+                .replacingOccurrences(of: "var", with: "")
+                .components(separatedBy: ";")
+                .filter { !$0.isEmpty }
+        for component in components {
+            let keyValue = component.components(separatedBy: "=")
+            guard keyValue.count == 2 else { fatalError("Format is not supported") }
+            if let key = keyValue.first?.removingAllWhitespaces(),
+                let value = keyValue.last?.removingAllWhitespaces() {
+                result[Settings(rawValue: key)!] = Int(value)!
+            }
+        }
+        
+        return result
+    }
+    
+    func removingAllWhitespaces() -> String {
+        return removingCharacters(from: .whitespacesAndNewlines)
+    }
+    
+    func removingCharacters(from set: CharacterSet) -> String {
+        var newString = self
+        newString.removeAll { char -> Bool in
+            guard let scalar = char.unicodeScalars.first else { return false }
+            return set.contains(scalar)
+        }
+        return newString
+    }
+}
+
+enum Settings: String {
+    case Resolution = "resolution"
+    case Mode = "mode"
+    case OSDEnable = "OSDEnable"
+    case ResolutionSub = "resolutionsub"
+    case SubEncFramerate = "sub_enc_framerate"
+    case Bright = "vbright"
+    case Saturation = "vsaturation"
+    case bitrate = "enc_bitrate"
+    case Hue = "vhue"
+    case Flip = "flip"
+    case IRcut = "ircut"
+    case Speed = "speed"
+    case Framerate = "enc_framerate"
+    case Contrast = "vcontrast"
+}
